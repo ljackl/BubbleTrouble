dfea9fd5d10a7d2aa2d80885cfaae4c1ddbbb848
ottowao <101082724@student.swin.edu.au>
2018-05-05 19:12:27 +1000

    CLion Unit Testing Intergration

---
+++ src/TestClass.hpp
@@ -0,0 +1,22 @@
+//
+// Created by pat on 5/05/18.
+// Test class to setup unit testing
+// For CLion see:
+// https://blog.jetbrains.com/clion/2017/03/to-catch-a-clion/
+// https://www.jetbrains.com/help/clion/catch-tests-support.html
+//
+
+#ifndef BUBBLETROUBLE_TESTCLASS_H
+#define BUBBLETROUBLE_TESTCLASS_H
+
+#ifdef _WIN32
+#include "stdafx.h"
+#endif
+
+class TestClass {
+public:
+    int add(int a, int b);
+};
+
+
+#endif //BUBBLETROUBLE_TESTCLASS_H
---
+++ unit_tests/Test.cpp
@@ -0,0 +1,18 @@
+//
+// Created by pat on 5/05/18.
+//
+
+#ifdef _WIN32
+#include "stdafx.h"
+#endif
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../src/TestClass.hpp"
+
+TEST_CASE("addition") {
+    TestClass test;
+    REQUIRE(test.add(2, 3) == 5);
+    REQUIRE(test.add(-1, 1) == 0);
+    REQUIRE(test.add(2, 4) == 6);
+}
